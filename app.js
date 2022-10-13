bc5faefcda13b3452145964e08f6341bc6b1cb33
Nicholas Kallas <kallasnicholas@gmail.com>
2022-10-13 13:02:28 -0500

    Fixed times and dates so that everything is now displayed in the local time of the city that is searched

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 "use strict";
 
-async function getWeather() {
+async function getWeather(measurement, windMeasure) {
   const city = document.querySelector(".search-input").value;
   const key = "9826289e241cb82fcf776863599b157f";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=imperial`;
+  const key2 = "W78G9X4OFKUX";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=${measurement}`;
   const response = await fetch(url);
   console.log(response);
   const data = await response.json();
   console.log(data);
+  const lat = data.coord.lat;
+  const lng = data.coord.lon;
+  const url2 = `http://api.timezonedb.com/v2.1/get-time-zone?key=${key2}&format=json&by=position&lat=${lat}&lng=${lng}`;
+  const response2 = await fetch(url2);
+  console.log(response2);
+  const timeZoneData = await response2.json();
+  console.log(timeZoneData);
 
   function displayWeatherInfo() {
     const infoCon = document.querySelector(".info-con");
@@ -19,7 +27,20 @@ async function getWeather() {
     const currentDate = document.createElement("time");
     const currentTime = document.createElement("time");
     const weatherCard = document.createElement("div");
+    const weatherInfoCon = document.createElement("div");
     const summaryCon = document.createElement("div");
+    const formattedDateTime = timeZoneData.formatted;
+    const formattedDateTimeArr = formattedDateTime.split(" ");
+    const dateStr = formattedDateTimeArr[0];
+    const splitDate = dateStr.split("-").join(", ");
+    const timeStr = formattedDateTimeArr[1];
+    const splitTime = timeStr.split(":").join(", ");
+    const dateTimeStr = splitDate + ", " + splitTime;
+    const dateTimeStrArr = dateTimeStr.split(",").map(Number);
+    dateTimeStrArr[1] = dateTimeStrArr[1] - 1;
+    console.log(dateTimeStrArr);
+    const testDate = new Date(...dateTimeStrArr);
+    console.log(testDate.getDay());
 
     const temp = document.createElement("div");
     const weatherIcon = document.createElement("i");
@@ -45,19 +66,22 @@ async function getWeather() {
     const maxTempHeading = document.createElement("h2");
     const sunriseCon = document.createElement("div");
     const sunriseHeading = document.createElement("h2");
+    const timezone = data.timezone;
     const sunrise = document.createElement("time");
-    const sunriseTime = new Date(data.sys.sunrise * 1000);
+    const sunriseTime = new Date((timezone + data.sys.sunrise) * 1000);
     const sunsetCon = document.createElement("div");
     const sunsetHeading = document.createElement("h2");
     const sunset = document.createElement("time");
-    const sunsetTime = new Date(data.sys.sunset * 1000);
+    const sunsetTime = new Date((timezone + data.sys.sunset) * 1000);
     const windCon = document.createElement("div");
     const windHeading = document.createElement("h2");
     const wind = document.createElement("div");
+    const today = new Date();
+    const localDay = new Date(data.dt * 1000);
+    console.log(localDay);
 
     cityName.textContent = data.name;
     // setting current time and date
-    const today = new Date();
     const daylist = [
       "Sunday",
       "Monday",
@@ -82,10 +106,12 @@ async function getWeather() {
       "November",
       "December",
     ];
-    currentDate.textContent = `${daylist[today.getDay()]}, ${
-      monthList[today.getMonth()]
-    } ${today.getDate()}, ${today.getFullYear()}`;
-    currentTime.textContent = `${today.getHours()}:${today.getMinutes()}`;
+    currentDate.textContent = `${daylist[testDate.getDay()]}, ${
+      monthList[testDate.getMonth()]
+    } ${testDate.getDate()}, ${testDate.getFullYear()}`;
+    currentTime.textContent = `${testDate.getHours()}:${
+      (testDate.getMinutes() < 10 ? "0" : "") + testDate.getMinutes()
+    }`;
     temp.textContent = data.main.temp;
 
     // setting weather icon
@@ -114,7 +140,7 @@ async function getWeather() {
     feelsLikeHeading.textContent = "Feels Like";
     feelsLike.textContent = `${Math.trunc(data.main.feels_like)}°`;
     windHeading.textContent = "Wind";
-    wind.textContent = `${Math.trunc(data.wind.speed)} mph`;
+    wind.textContent = `${Math.trunc(data.wind.speed)}${windMeasure}`;
     humidityHeading.textContent = "Humidity";
     humidity.textContent = `${data.main.humidity}%`;
     pressureHeading.textContent = "Pressure";
@@ -124,15 +150,22 @@ async function getWeather() {
     maxTempHeading.textContent = "Max Temp";
     maxTemp.textContent = `${Math.trunc(data.main.temp_max)}°`;
     sunriseHeading.textContent = "Sunrise";
-    sunrise.textContent = `${sunriseTime.getHours()}:${sunriseTime.getMinutes()}`;
+    sunrise.textContent = `${sunriseTime.getUTCHours()}:${
+      (sunriseTime.getUTCMinutes() < 10 ? "0" : "") +
+      sunriseTime.getUTCMinutes()
+    }`;
     sunsetHeading.textContent = "Sunset";
-    sunset.textContent = `${sunsetTime.getHours()}:${sunsetTime.getMinutes()}`;
+    console.log(sunsetTime.getUTCMinutes() < 10);
+    sunset.textContent = `${sunsetTime.getUTCHours()}:${
+      (sunsetTime.getUTCMinutes() < 10 ? "0" : "") + sunsetTime.getUTCMinutes()
+    }`;
 
     // adding classes
     weatherHeader.classList.add("weather-header");
     cityName.classList.add("city-name");
     dateTime.classList.add("date-time");
     weatherCard.classList.add("weather-card");
+    weatherInfoCon.classList.add("weather-info-con");
     summaryCon.classList.add("summary-con");
     weatherIcon.classList.add("weather-icon");
     temp.classList.add("temp");
@@ -182,9 +215,10 @@ async function getWeather() {
     });
 
     // appending elements
-    infoCon.append(weatherHeader, weatherCard);
+    infoCon.append(weatherCard);
     weatherHeader.append(cityName, dateTime);
-    weatherCard.append(summaryCon, detailsCon);
+    weatherCard.append(weatherHeader, weatherInfoCon);
+    weatherInfoCon.append(summaryCon, detailsCon);
     summaryCon.append(weatherIcon, temp, conditionsDesc);
     detailsCon.append(
       feelsLikeCon,
@@ -210,7 +244,17 @@ async function getWeather() {
   displayWeatherInfo();
 }
 
-document.querySelector(".search-btn").addEventListener("click", getWeather);
+document.querySelector(".search-btn").addEventListener("click", () => {
+  getWeather("imperial", "mph");
+});
+
+document.querySelector(".cels-btn").addEventListener("click", () => {
+  getWeather("metric", "m/s");
+});
+
+document.querySelector(".fahr-btn").addEventListener("click", () => {
+  getWeather("imperial", "mph");
+});
 
 function clearContainer(container) {
   while (container.firstChild) {
